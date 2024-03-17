0b90210b7dbfbf45f824ce0c9dd1bb3eb309537f
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2024-03-17 17:52:04 +0300

    finally4

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import store from './redux/redux-store'
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter  basename ="/">
+            <HashRouter  basename ={"/"}>
                 <App/>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     </React.StrictMode>
 );
