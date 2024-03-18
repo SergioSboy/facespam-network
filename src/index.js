a67068fd04f290a6e005e7d90c648844dcb61e8e
SergioSboy <118937788+SergioSboy@users.noreply.github.com>
2024-03-18 16:25:00 +0300

    404 error

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import store from './redux/redux-store'
 import {Provider} from "react-redux";
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <HashRouter>
+            <HashRouter basename={'/'}>
                 <App/>
             </HashRouter>
         </Provider>
